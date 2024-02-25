6083cb3702a7d237911173e51095fdcbf32d36a1
DelvinCappinova <delvin.cappinova@ti.ukdw.ac.id>
2024-02-26 00:10:19 +0700

    done

---
+++ 1.py
@@ -0,0 +1,5 @@
+suhu = float(input("Suhu anda :"))
+if suhu >= 38:
+    print ("Kamu demam")
+else:
+    print ("Kamu tidak demam")
---
+++ 2.py
@@ -0,0 +1,7 @@
+bilangan = int(input("input bilangan anda:"))
+if bilangan == 0:
+    print ("Nol")
+elif bilangan >0 :
+    print ("Bilangan Positif")
+elif bilangan <0 :
+    print ("Bilangan Negatif")
